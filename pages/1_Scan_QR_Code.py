619dccd94209f0171bf6a900d1ddef71dd4b433f
Mehdi <mehdi9155@gmail.com>
2025-11-27 11:45:32 +0100

    New

--- pages/1_Scan_QR_Code.py
+++ pages/1_Scan_QR_Code.py
@@ -1,43 +1,16 @@
+import cv2
+import numpy as np
 import streamlit as st
-import streamlit.components.v1 as components
 
-st.set_page_config(page_title="Scan QR Code", page_icon="📷", layout="wide")
+image = st.camera_input("Show QR code")
 
-st.title("📷 Scan QR Code")
-st.write("Scannez un QR code pour charger les données de la batterie.")
+if image is not None:
+    bytes_data = image.getvalue()
+    cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
 
-qr_value = st.text_input("QR détecté", key="qr_value", label_visibility="collapsed")
+    detector = cv2.QRCodeDetector()
 
-html_code = """
-<script src="https://unpkg.com/html5-qrcode"></script>
+    data, bbox, straight_qrcode = detector.detectAndDecode(cv2_img)
 
-<div id="reader" style="width: 500px; margin: auto"></div>
-
-<script>
-function onScanSuccess(decodedText, decodedResult) {
-    // Send decoded text to Streamlit input field
-    const inputBox = window.parent.document.querySelector('input[id="qr_value"]');
-    inputBox.value = decodedText;
-    inputBox.dispatchEvent(new Event('input', { bubbles: true }));
-}
-
-function onScanFailure(error) {
-    // Ignore scan failures
-}
-
-let html5QrcodeScanner = new Html5QrcodeScanner(
-    "reader",
-    { fps: 10, qrbox: { width: 250, height: 250 } },
-    false
-);
-
-html5QrcodeScanner.render(onScanSuccess, onScanFailure);
-</script>
-"""
-
-components.html(html_code, height=600)
-    
-if qr_value:
-    st.success(f"QR Code détecté : {qr_value}")
-    st.session_state["battery_id"] = qr_value
-    st.page_link("pages/2_Battery_Data.py", label="➡️ Voir les données")
+    st.write("Here!")
+    st.write(data)
